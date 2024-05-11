9a9d48ab8753b01a966f97c108704094e0301911
Train Barista <tranbao28923@gmail.com>
2024-05-11 09:11:18 +0700

    fix . file

--- js/home.js
+++ js/home.js
@@ -24,63 +24,54 @@ function hideInput() {
   }
 }
 
+const button = document.getElementById("singin");
+const message = document.getElementById("singup");
 
+button.addEventListener("click", function () {
+  button.style.display = "none";
 
-const button = document.getElementById('singin');
-const message = document.getElementById('singup');
+  message.style.display = "block";
 
-button.addEventListener('click', function() {
- 
-  button.style.display = 'none';
-
- 
-  message.style.display = 'block';
-
-
-  message.textContent = 'Thank you for subscribing!';
+  message.textContent = "Thank you for subscribing!";
 });
 
+const image = document.querySelector("img");
+const tooltip = document.querySelector("#tooltip");
 
-const image = document.querySelector('img');
-const tooltip = document.querySelector('#tooltip');
-
-image.addEventListener('mouseover', function() {
-  tooltip.style.display = 'block';
+image.addEventListener("mouseover", function () {
+  tooltip.style.display = "block";
 });
 
-image.addEventListener('mouseout', function() {
-  tooltip.style.display = 'none';
+image.addEventListener("mouseout", function () {
+  tooltip.style.display = "none";
 });
-
 
 // Thêm vào giỏ
-var add_cart = document.getElementsByClassName('button[data-id]');
+var add_cart = document.getElementsByClassName("button[data-id]");
 for (var i = 0; i < add_cart.length; i++) {
   var add = add_cart[i];
   add.addEventListener("click", function (event) {
-
     var button = event.target;
     var product = button.parentElement.parentElement;
-    var img = product.parentElement.getElementsByClassName("img-prd")[0].src
-    var title = product.getElementsByClassName("o1-content")[0].innerText
-    var price = product.getElementsByClassName("o1-content1")[0].innerText
-    addItemToCart(title, price, img)
+    var img = product.parentElement.getElementsByClassName("img-prd")[0].src;
+    var title = product.getElementsByClassName("o1-content")[0].innerText;
+    var price = product.getElementsByClassName("o1-content1")[0].innerText;
+    addItemToCart(title, price, img);
     // Khi thêm sản phẩm vào giỏ hàng thì sẽ hiển thị modal
     modal.style.display = "block";
-    
-    updatecart()
-  })
+
+    updatecart();
+  });
 }
 
 function addItemToCart(title, price, img) {
-
-  var cartItems = document.getElementsByClassName('o1-img')[0]
-  var cart_title = cartItems.getElementsByClassName('article o1')
+  var cartItems = document.getElementsByClassName("o1-img")[0];
+  var cart_title = cartItems.getElementsByClassName("article o1");
   // Nếu title của sản phẩm bằng với title mà bạn thêm vao giỏ hàng thì sẽ thông cho user.
   for (var i = 0; i < cart_title.length; i++) {
     if (cart_title[i].innerText == title) {
-      alert('Sản Phẩm Đã Có Trong Giỏ Hàng')
-      return
+      alert("Sản Phẩm Đã Có Trong Giỏ Hàng");
+      return;
     }
   }
 
@@ -93,19 +84,23 @@ function addItemToCart(title, price, img) {
   <div class="cart-quantity cart-column">
       <input class="cart-quantity-input" type="number" value="1">
       <button class="btn btn-danger" type="button">Xóa</button>
-  </div>`
-  cartRow.innerHTML = cartRowContents
-  cartItems.append(cartRow)
-  cartRow.getElementsByClassName('button[data-id]')[0].addEventListener('click', function () {
-    var button_remove = event.target
-    button_remove.parentElement.parentElement.remove()
-    updatecart()
-  })
-  cartRow.getElementsByClassName('o1-img')[0].addEventListener('change', function (event) {
-    var input = event.target
-    if (isNaN(input.value) || input.value <= 0) {
-      input.value = 1;
-    }
-    updatecart()
-  })
+  </div>`;
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
+  cartRow
+    .getElementsByClassName("button[data-id]")[0]
+    .addEventListener("click", function () {
+      var button_remove = event.target;
+      button_remove.parentElement.parentElement.remove();
+      updatecart();
+    });
+  cartRow
+    .getElementsByClassName("o1-img")[0]
+    .addEventListener("change", function (event) {
+      var input = event.target;
+      if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+      }
+      updatecart();
+    });
 }
